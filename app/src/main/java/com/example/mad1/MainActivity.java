ca779989d2360352664eae53ffe347e98e776e49
nthnua <97590341+nthnua@users.noreply.github.com>
2022-03-04 11:01:37 +0530

    Mad2 - CheckBoxs

--- app/src/main/java/com/example/mad1/MainActivity.java
+++ app/src/main/java/com/example/mad1/MainActivity.java
@@ -2,26 +2,29 @@ package com.example.mad1;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.CheckBox;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button addButton;
-    EditText t1,t2;
+    Button resButton;
+    CheckBox c1,c2,c3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addButton = findViewById(R.id.AddButton);
-        addButton.setOnClickListener(this::showResult);
+        resButton = findViewById(R.id.resButton);
+        resButton.setOnClickListener(this::showResult);
     }
     void showResult(View v){
-        t1 = findViewById(R.id.no1);
-        t2 = findViewById(R.id.no2);
-        Double sum = Double.parseDouble(t1.getText().toString()) +Double.parseDouble(t2.getText().toString());
-        Toast.makeText(getApplicationContext(),"Sum is: " + sum,Toast.LENGTH_SHORT).show();
+        CheckBox[] chks = {findViewById(R.id.checkBox1),findViewById(R.id.checkBox2),findViewById(R.id.checkBox3)};
+        StringBuilder result = new StringBuilder("Selected: ");
+        for(CheckBox chk:chks){
+            result.append(chk.isChecked() ? chk.getText() + " " : " ");
+        }
+        Toast.makeText(getApplicationContext(), result,Toast.LENGTH_SHORT).show();
     }
 }
