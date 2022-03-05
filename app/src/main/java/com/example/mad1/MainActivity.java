d2bed70856719011757fa28dde45d478ff41710e
nthnua <97590341+nthnua@users.noreply.github.com>
2022-03-05 12:25:08 +0530

    Mad3 - Random colors

--- app/src/main/java/com/example/mad1/MainActivity.java
+++ app/src/main/java/com/example/mad1/MainActivity.java
@@ -1,30 +1,30 @@
 package com.example.mad1;
 
+import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button resButton;
-    CheckBox c1,c2,c3;
-
+    ConstraintLayout c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         resButton = findViewById(R.id.resButton);
         resButton.setOnClickListener(this::showResult);
+       c = findViewById(R.id.c);
     }
     void showResult(View v){
-        CheckBox[] chks = {findViewById(R.id.checkBox1),findViewById(R.id.checkBox2),findViewById(R.id.checkBox3)};
-        StringBuilder result = new StringBuilder("Selected: ");
-        for(CheckBox chk:chks){
-            result.append(chk.isChecked() ? chk.getText() + " " : " ");
-        }
-        Toast.makeText(getApplicationContext(), result,Toast.LENGTH_SHORT).show();
+        Random r = new Random();
+        c.setBackgroundColor(Color.rgb(r.nextInt(256),r.nextInt(256),r.nextInt(256)));
     }
 }
