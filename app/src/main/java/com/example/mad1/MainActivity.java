7c49823df7f2e623141cb2e05d42694a66e63e6c
nthnua <97590341+nthnua@users.noreply.github.com>
2022-03-07 12:48:04 +0530

    Mad4 -  OptionsMenu

--- app/src/main/java/com/example/mad1/MainActivity.java
+++ app/src/main/java/com/example/mad1/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.mad1;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -13,18 +18,32 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button resButton;
-    ConstraintLayout c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resButton = findViewById(R.id.resButton);
-        resButton.setOnClickListener(this::showResult);
-       c = findViewById(R.id.c);
     }
-    void showResult(View v){
-        Random r = new Random();
-        c.setBackgroundColor(Color.rgb(r.nextInt(256),r.nextInt(256),r.nextInt(256)));
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater m = getMenuInflater();
+        m.inflate(R.menu.m1,menu);
+        return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.i1:
+                Intent i1 = new Intent(this,MainActivity2.class);
+                startActivity(i1);
+                break;
+            case R.id.i2:
+                Intent i2 = new Intent(this,MainActivity3.class);
+                startActivity(i2);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
